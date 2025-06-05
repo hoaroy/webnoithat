7a79a66fcad1beff8955a2ebaea603b78bb4f1f3
hoaroy <101579764+hoaroy@users.noreply.github.com>
2025-06-05 18:41:44 +0700

    Update 2025_02_06_134338_create_product_table.php

--- database/migrations/2025_02_06_134338_create_product_table.php
+++ database/migrations/2025_02_06_134338_create_product_table.php
@@ -13,21 +13,23 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
-            $table->id('product_id');
-            $table->string('product_name');
-            $table->string('product_slug')->unique();
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('category_id')->on('category')->onUpdate('cascade')->onDelete('cascade');
-            $table->string('product_desc');
-            $table->bigInteger('product_price');
-            $table->bigInteger('product_price_sale');
-            $table->integer('product_quantity');
-            $table->integer('product_view');
-            $table->integer('product_sold');
-            $table->tinyInteger('product_status',0);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('product')) {
+            Schema::create('product', function (Blueprint $table) {
+                $table->id('product_id');
+                $table->string('product_name');
+                $table->string('product_slug')->unique();
+                $table->unsignedBigInteger('category_id');
+                $table->foreign('category_id')->references('category_id')->on('category')->onUpdate('cascade')->onDelete('cascade');
+                $table->string('product_desc');
+                $table->bigInteger('product_price');
+                $table->bigInteger('product_price_sale');
+                $table->integer('product_quantity');
+                $table->integer('product_view');
+                $table->integer('product_sold');
+                $table->tinyInteger('product_status',0);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
