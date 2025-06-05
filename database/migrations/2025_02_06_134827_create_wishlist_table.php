3a1084a889a936e07ac8c3550afc90daaf68b16f
hoaroy <101579764+hoaroy@users.noreply.github.com>
2025-06-05 18:42:21 +0700

    Update 2025_02_06_134827_create_wishlist_table.php

--- database/migrations/2025_02_06_134827_create_wishlist_table.php
+++ database/migrations/2025_02_06_134827_create_wishlist_table.php
@@ -13,14 +13,16 @@ class CreateWishlistTable extends Migration
      */
     public function up()
     {
-        Schema::create('wishlist', function (Blueprint $table) {
-            $table->id('wishlist_id');
-            $table->unsignedBigInteger('pro_id');
-            $table->foreign('pro_id')->references('product_id')->on('product')->onUpdate('cascade')->onDelete('cascade');
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('wishlist')) {
+            Schema::create('wishlist', function (Blueprint $table) {
+                $table->id('wishlist_id');
+                $table->unsignedBigInteger('pro_id');
+                $table->foreign('pro_id')->references('product_id')->on('product')->onUpdate('cascade')->onDelete('cascade');
+                $table->unsignedBigInteger('user_id');
+                $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
