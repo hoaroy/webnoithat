31ac63043d9e029c30ba7c77841a475651c9380e
hoaroy <101579764+hoaroy@users.noreply.github.com>
2025-06-05 18:39:51 +0700

    Update 2025_02_06_000000_create_users_table.php

--- database/migrations/2025_02_06_000000_create_users_table.php
+++ database/migrations/2025_02_06_000000_create_users_table.php
@@ -13,16 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('level',0);
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('username')->unique();
+                $table->string('email')->unique();
+                $table->string('password');
+                $table->tinyInteger('level',0);
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
